f4020675d85593e5707bfd32c1c97cbb78cbb50e
AMAR KUMAR <155427107+AmarKumar2005@users.noreply.github.com>
2025-06-01 16:12:55 +0000

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const images = ["../assets/Front.jpg", "assets/Middle01.jpg", "assets/Back.jpg"]; // Add your image names
+const images = ["assets/Front.jpg", "assets/Middle01.jpg", "assets/Back.jpg"]; // Add your image names
 let current = 0;
 
 const slideshow = document.getElementById("slideshow");
