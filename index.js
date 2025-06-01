59f56c4dde622f0949cfc8261242c8e36b4b3fea
AMAR KUMAR <155427107+AmarKumar2005@users.noreply.github.com>
2025-06-01 14:05:47 +0000

    slideshow

---
+++ index.js
@@ -0,0 +1,25 @@
+const images = ["../assets/Front.jpg", "../assets/Middle01.jpg", "../assets/Back.jpg"]; // Add your image names
+let current = 0;
+
+const slideshow = document.getElementById("slideshow");
+const animationEl = document.getElementById("animation");
+
+function updateSlideshow() {
+  // Change image
+  slideshow.src = images[current];
+
+  // Show animation shape
+  animationEl.classList.add("show");
+
+  // Hide it after 2 seconds
+  setTimeout(() => {
+    animationEl.classList.remove("show");
+  }, 2000);
+
+  // Next image index
+  current = (current + 1) % images.length;
+}
+
+// Start on load
+updateSlideshow();
+setInterval(updateSlideshow, 3000);
